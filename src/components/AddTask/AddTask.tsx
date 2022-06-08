e6c67eac38a69a42ce27880872b3e4e4ca43222d
alysonbarreto02 <alysonbarretito2123@gmail.com>
2022-06-08 13:28:45 -0300

    tasknoexist implementado

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import { GrAddCircle } from "react-icons/gr";
 import CreatedTasks from "../ZoneTask/CreatedTasks";
 import Task from "../ZoneTask/Task";
+import TaksNoExists from "../ZoneTask/TasksNoExists";
 
 
 export default function AddTask() {
 
-    const [moreTask, setMoreTask] = useState(["Top"])
+    const [moreTask, setMoreTask] = useState([])
 
     function showMoreTaks(e) {
         e.preventDefault()
@@ -23,25 +24,31 @@ export default function AddTask() {
     }
 
     return (
-        <div className="flex-col justify-center items-center -mt-7">
-            <div className="flex w-full justify-center">
-                <form onSubmit={showMoreTaks} className="flex w-full justify-center">
-                    <input value={newTask} onChange={changeNewTask} type="text" placeholder="Adicione uma nova tarefa" className="text-white px-4 w-2/4 h-14 bg-zinc-600 rounded-lg" />
-                    <button className="flex  justify-center items-center bg-blue-500 w-24 h-14 ml-3 rounded-xl text-white font-medium">Criar <div className="ml-2"><GrAddCircle /></div> </button>
-                </form>
-            </div>
+        <>
+            <div className="flex-col justify-center items-center -mt-7">
+                <div className="flex w-full justify-center">
+                    <form onSubmit={showMoreTaks} className="flex w-full justify-center">
+                        <input required value={newTask} onChange={changeNewTask} type="text" placeholder="Adicione uma nova tarefa" className="text-white px-4 w-2/4 h-14 bg-zinc-600 rounded-lg" />
+                        <button className="flex  justify-center items-center bg-blue-500 w-24 h-14 ml-3 rounded-xl text-white font-medium">Criar <div className="ml-2"><GrAddCircle /></div> </button>
+                    </form>
+                </div>
 
-            <CreatedTasks />
-            
-            {moreTask.map((moreTask, index) => {
-                return (
-                    <Task
-                        description={moreTask}
-                        key={`index-list-task-${index}`}
-                    />
-                )
-            }
-            )}
-        </div>
+                <CreatedTasks />
+
+                {moreTask.map((moreTask, index) => {
+                    return (
+                        <Task
+                            description={moreTask}
+                            key={`index-list-task-${index}`}
+                        />
+                    )
+                }
+                )}
+            </div>
+            <div className="flex-col justify-center items-center mt-64">
+                {moreTask.length === 0 &&
+                    <TaksNoExists />}
+            </div>
+        </>
     )
 }
